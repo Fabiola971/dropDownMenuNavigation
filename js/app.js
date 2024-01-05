d594d79f243faf7b62756cbf956f6a444e35faf8
Fabiola971 <vanillesdesiles971@yahoo.fr>
2024-01-05 08:08:02 +0100

    drop down menu mobile

--- js/app.js
+++ js/app.js
@@ -19,3 +19,14 @@ closeMenu.addEventListener('click', ()=>{
     menuCollapse.classList.toggle('block');
 
 });
+
+// SUBMENU APPEAR
+
+const submenu = document.querySelectorAll('.submenu');
+console.log(submenu);
+
+const linkContainerItem = document.querySelectorAll('.linkContainer li');
+console.log(linkContainerItem);
+
+
+
